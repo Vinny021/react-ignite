46eff4c60b2a6ab0696f92c665d3e6eb44f6a05c
Vinny021 <viniciusbarbosa021@gmail.com>
2022-01-28 18:30:12 -0300

    Component State

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,32 +1,20 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+const repositoryMock = {
+  name: "unform",
+  description: "Unform Repository",
+  link: "https://github.com/unform/unform",
+};
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1> Repository List</h1>
 
       <ul>
-        <RepositoryItem
-          repository={{
-            name: " Repo 1",
-            description: "First Repo from the list",
-            link: "https://github.com",
-          }}
-        />
-        <RepositoryItem
-          repository={{
-            name: " Repo 2",
-            description: "Second Repo from the list",
-            link: "https://github.com",
-          }}
-        />
-        <RepositoryItem
-          repository={{
-            name: " Repo 3",
-            description: "Third Repo from the list",
-            link: "https://github.com",
-          }}
-        />
+        <RepositoryItem repository={repositoryMock} />
+        <RepositoryItem repository={repositoryMock} />
+        <RepositoryItem repository={repositoryMock} />
       </ul>
     </section>
   );
