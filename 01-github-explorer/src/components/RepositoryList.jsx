1febb00d2561a9c9d533d3362f73751c75715d63
Vinny021 <viniciusbarbosa021@gmail.com>
2022-02-01 18:15:02 -0300

    Fetch Data + Style

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from "react";
+import "../styles/repositories.scss";
 
 const repositoryMock = {
   name: "unform",
@@ -7,6 +9,16 @@ const repositoryMock = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  console.log(repositories);
+
   return (
     <section className="repository-list">
       <h1> Repository List</h1>
